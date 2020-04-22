3da790591d30256df63ee149eb392773b7d1e2fd
ldipasquale <yo@lucianodipasquale.me>
2020-04-22 14:03:11 -0300

    remove

--- src/config/api.js
+++
@@ -1,4 +0,0 @@
-export default {
-  baseURL: 'http://192.168.0.62:3000',
-  baseURL: 'https://kinder-kids-server.herokuapp.com',
-}
--- src/config/aws.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  accessKey: 'AKIAJC75FUEGZAYQYAJQ',
-  secretKey: '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',
-  region: 'us-east-1',
-  bucket: 'kinder-kids-files',
-}
